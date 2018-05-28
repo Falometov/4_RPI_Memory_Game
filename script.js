cb4c7157d739388d35b3b4d9b8ddf1f756eb6cd8
falometov <vadfalo@gmail.com>
2018-05-28 18:02:18 +0300

    added cards

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-var rows = 4,
-	columns,
-	diff = 0,
-	startButton = document.getElementById("start");
+var diff = null,
+	startButton = document.getElementById("start"),
+	backImage = 1,
+	myTimer,
+	game = false;
+
+function changeBackground(obj){
+	obj.value && (document.body.style.background = "url("+obj.value+") no-repeat fixed center top 0% / 100% auto");
+}
+
+function setDifficulty(obj){
+	diff = obj.value;
+	if (diff === 16){
+		let workSpace = document.getElementById("");
+	}
+}
+
+function setBack(obj){
+	backImage = obj.value;
+}
+
+var cardsList = ["1","2","3","4","5","6","7","8","9","10","11","12"];
 
 var delay = 100,
 	i = 0,
@@ -24,78 +42,93 @@ var delay = 100,
 				}
 			}
 		}
-		var edit = document.getElementById("timer");
-		var current_time = h+" : "+m+" : "+s+" . "+ms;
+		let edit = document.getElementById("timer"),
+			current_time = h+" : "+m+" : "+s+" . "+ms;
 		edit.value = current_time;
 		timer();			
 	};
 
 function timer(){
-	setTimeout(startTimer, delay);
+	myTimer = setTimeout(startTimer, delay);
+}
+
+function resetGame(){
+	window.location.reload();
 }
 
 function startGame(event){
-	if (diff == 0){
-		alert("Please, choose difficulty level!");
-	}else{
-		event.preventDefault();
-		if (!timerStatus){
-			timerStatus = true;
-			timer();
-		}
-		var cardsList = [
-			{'id':"1",'img':"images/1.jpg"},
-			{'id':"2",'img':"images/2.jpg"},
-			{'id':"3",'img':"images/3.jpg"},
-			{'id':"4",'img':"images/4.jpg"},
-			{'id':"5",'img':"images/5.jpg"},
-			{'id':"6",'img':"images/6.jpg"},
-			{'id':"7",'img':"images/7.jpg"},
-			{'id':"8",'img':"images/8.jpg"},
-			{'id':"9",'img':"images/9.jpg"},
-			{'id':"10",'img':"images/10.jpg"},
-			{'id':"11",'img':"images/11.jpg"},
-			{'id':"12",'img':"images/12.jpg"}],
-			firstCard = '',
-			secondCard = '',
-			count = 0,
-			arrayLength = rows * columns,
-			cards = new Array(arrayLength),
-			cardsArray = cardsList.concat(cardsList);
-
-		for (var i = 0; i < arrayLength/2; i++){
-			cards[i] = cardsArray[i];
-			cards[i+(arrayLength/2)] = cardsArray[i+(cardsArray.length/2)];
-		}
-
-		function shuffle(array){
-			let indexBefore = array.length,
-				temp,
-				indexAfter;
-			while (indexBefore > 0){
-				indexAfter = Math.floor(indexBefore * Math.random);
-				indexBefore--;
-
-				temp = array[indexBefore];
-				array[indexBefore] = array[indexAfter];
-				array[indexAfter] = temp;		
+	if (!game){
+		if (diff == null){
+			alert("Please, choose difficulty level!");
+		}else{
+			event.preventDefault();
+			game = true;
+			if (!timerStatus){
+				timerStatus = true;
+				timer();
 			}
-			return(array);
-		}
+			var firstCard = '',
+				secondCard = '',
+				arrayLength = diff,
+				cards = new Array(arrayLength),
+				cardsArray = cardsList.concat(cardsList);
 
-		shuffle(cards);
-		
-		
+			for (var i = 0; i < arrayLength/2; i++){
+				cards[i] = cardsArray[i];
+				cards[i+(arrayLength/2)] = cardsArray[i+(cardsArray.length/2)];
+			}
+
+			function shuffle(array){
+				let indexBefore = array.length,
+					temp,
+					indexAfter;
+				while (indexBefore > 0){
+					indexAfter = Math.floor(indexBefore * Math.random());
+					indexBefore--;
+
+					temp = array[indexBefore];
+					array[indexBefore] = array[indexAfter];
+					array[indexAfter] = temp;		
+				}
+				return(array);
+			}
+
+			shuffle(cards);
+
+			var cardsNum = 0,
+				previousTarget = null,
+				workSpace = document.getElementById("work_space"),
+				workDivs = document.createElement("section");
+			workDivs.classList.add("work_divs");
+			workDivs.style.heigh = 830+"px";
+
+			i = 0;
+			while (i < cards.length){
+				var cardImage = document.createElement("div");
+				cardImage.classList.add("card_img");
+				cardImage.dataset.id = cards[i];
+
+				var cardBack = document.createElement("div");
+				cardBack.classList.add("card_back");
+				if (backImage === 1){
+					cardBack.classList.add("first_back");
+				}else{
+					cardBack.classList.add("second_back");
+				}
+
+				var cardFront = document.createElement("div");
+				cardFront.classList.add("card_front");
+				console.log(i);
+				cardFront.style.backgroundImage = `url(images/${cards[i]}.jpg)`;
+
+				workDivs.appendChild(cardImage);
+				cardImage.appendChild(cardFront);
+				cardImage.appendChild(cardBack);
+				i++;
+			}
+			workSpace.appendChild(workDivs);
+		}
 	}
 }
 
 startButton.addEventListener("click", startGame);
-
-function changeBackground(obj){
-	obj.value && (document.body.style.background = 'url('+obj.value+') no-repeat fixed center top 0% / 100% auto');
-}
-
-function setDifficulty(obj){
-	diff = obj.value;
-	columns = diff/rows;
-}
